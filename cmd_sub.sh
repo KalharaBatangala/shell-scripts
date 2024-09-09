e73b8d56d8ac283449f56e5a0b1adf69554e03d4
Kalhara Batangala <119377219+KalharaBatangala@users.noreply.github.com>
2024-09-09 12:18:08 +0530

    command substitution

---
+++ cmd_sub.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Command Substitution"
+
+#current_dir=`pwd`
+current_dir=$(pwd)
+echo "Current directory is :"$current_dir
+
---
+++ command_substitution.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Command Substitution"
+
+read -p "Username : " username
+read -sp "Password : " password
+echo ""
+echo "Username is : "$username "password is : "$password
+
